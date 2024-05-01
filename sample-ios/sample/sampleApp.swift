82c61d0d0f906480e296e110a7f46c4b7e46b061
takenaka <https://github.com/TakenokoTech>
2024-05-01 16:51:23 +0900

    add ios sample.

---
+++ sample-ios/sample/sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleApp.swift
+//  sample
+//
+//  Created by Takenoko on 2024/05/01.
+//
+
+import SwiftUI
+
+@main
+struct sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
